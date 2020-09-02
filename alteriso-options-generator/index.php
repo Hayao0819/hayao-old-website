492de8841ebd45de12236b8a36b06e138647d1c3
hayao <shun819.mail@gmail.com>
2020-09-02 18:26:05 +0900

    [update] : テーブルからflexにした

--- alteriso-options-generator/index.php
+++ alteriso-options-generator/index.php
@@ -17,62 +17,62 @@
             <form method="POST">
                 <h2>ビルド設定</h2>
 
-                <table>
+                <div class="box">
 
                     <!-- Plymouth ここから-->
-                    <tr>
-                        <td class="table_title"><h3>Plymouth</h3></td>
-                        <td>有効にすると起動時にアニメーションを表示します</td>
-                        <td>
+                    <div class="box_child">
+                        <p class="option_title"><h3>Plymouth</h3></p>
+                        <p class="option_description">有効にすると起動時にアニメーションを表示します</p>
+                        <p class="option_form">
                             <label for="plymouth_enable"><input type="radio" name="plymouth" value="enable" id="plymouth_enable" checked>有効</label>
                             <label for="plymouth_disable"><input type="radio" name="plymouth" value="disable" id="plymouth_disable">無効</label>
-                        </td>
-                    </tr>
+                        </p>
+                    </div>
                     <!-- Plymouth ここまで-->
 
                     <!-- クリーニング ここから-->
-                    <tr>
-                        <td class="table_title"><h3>クリーニング</h3></td>
-                        <td>ビルド中に不要なファイルを削除します</td>
-                        <td>
+                    <div class="box_child">
+                        <p class="option_title"><h3>クリーニング</h3></p>
+                        <p class="option_description">ビルド中に不要なファイルを削除します</p>
+                        <p class="option_form">
                             <label for="clean_enable"><input type="radio" name="clean" value="enable" id="clean_enable" checked>有効</label>
                             <label for="clean_disable"><input type="radio" name="clean" value="disable" id="clean_disable">無効</label>
-                        </td>
-                    </tr>
+                        </p>
+                    </div>
                     <!-- クリーニング ここまで-->
 
                     <!-- tarball ここから-->
-                    <tr>
-                        <td class="table_title"><h3>Tarball</h3></td>
-                        <td>Tarball形式でビルドします</td>
-                        <td>
+                    <div class="box_child">
+                        <p class="option_title"><h3>Tarball</h3></p>
+                        <p class="option_description">Tarball形式でビルドします</p>
+                        <p class="option_form">
                             <label for="tarball_enable"><input type="radio" name="tarball" value="enable" id="tarball_enable">有効</label>
                             <label for="tarball_disable"><input type="radio" name="tarball" value="disable" id="tarball_disable" checked>無効</label>
-                        </td>
-                    </tr>
+                        </p>
+                    </div>
                     <!-- tarball ここまで-->
 
 
 
                     <!-- アーキテクチャ ここから-->
-                    <tr>
-                        <td class="table_title"><h3>アーキテクチャ</h3></td>
-                        <td>ビルドするアニメーションを指定します</td>
-                        <td>
+                    <div class="box_child">
+                        <p class="option_title"><h3>アーキテクチャ</h3></p>
+                        <p class="option_description">ビルドするアニメーションを指定します</p>
+                        <p class="option_form">
                             <select name="architecture" id="architecture">
                                 <option value="i686">i686</option>
                                 <option value="x86_64" selected>x86_64</option>
                             </select>
-                        </td>
-                    </tr>
+                        </p>
+                    </div>
                     <!-- アーキテクチャ ここまで-->
 
 
                     <!-- 圧縮設定 ここから-->
-                    <tr>
-                        <td class="table_title"><h3>Squashfsの圧縮方式</h3></td>
-                        <td>Rootfsの圧縮方式を指定します</td>
-                        <td>
+                    <div class="box_child">
+                        <p class="option_title"><h3>Squashfsの圧縮方式</h3></p>
+                        <p class="option_description">Rootfsの圧縮方式を指定します</p>
+                        <p class="option_form">
                               <select name="sfs-comp-type" id="sfs-comp-type">
                                 <option value="gzip">gzip</option>
                                 <option value="lzma">lzma</option>
@@ -81,39 +81,40 @@
                                 <option value="xz">xz</option>
                                 <option value="zstd" selected>zstd</option>
                             </select>
-                        </td>
-                    </tr>
+                        </p>
+                    </div>
                     <!-- 圧縮設定 ここまで-->
 
                     <!-- ユーザー ここから-->
-                    <tr>
-                        <td class="table_title"><h3>ライブ環境ユーザー名</h3></td>
-                        <td>ライブ環境のユーザー名を指定します</td>
-                        <td><input type="text" id="username" placeholder="alter" value="alter"></td>
-                    </tr>
-                    <tr>
-                        <td class="table_title"><h3>ライブ環境パスワード</h3></td>
-                        <td>ライブ環境のユーザーのパスワードを設定します</td>
-                        <td><input type="password" id="password" placeholder="alter" value="alter"></td>
-                    </tr>
+                    <div class="box_child">
+                        <p class="option_title"><h3>ライブ環境ユーザー名</h3></p>
+                        <p class="option_description">ライブ環境のユーザー名を指定します</p>
+                        <p class="option_form"><input type="text" id="username" placeholder="alter" value="alter"></p>
+                    </div>
+                    <div class="box_child">
+                        <p class="option_title"><h3>ライブ環境パスワード</h3></p>
+                        <p class="option_description">ライブ環境のユーザーのパスワードを設定します</p>
+                        <p class="option_form"><input type="password" id="password" placeholder="alter" value="alter"></p>
+                    </div>
                     <!-- ユーザー ここまで-->
 
                     <!-- デバッグ用オプション ここから -->
-                    <tr>
-                        <td class="table_title"><h3>デバッグメッセージ</h3></td>
-                        <td>デバッグメッセージを表示します</td>
-                        <td>
+                    <div class="box_child">
+                        <p class="option_title"><h3>デバッグメッセージ</h3></p>
+                        <p class="option_description">デバッグメッセージを表示します</p>
+                        <p class="option_form">
                             <label for="debug_enable"><input type="radio" name="debug" value="enable" id="debug_enable">有効</label>
                             <label for="debug_disable"><input type="radio" name="debug" value="disable" id="debug_disable" checked>無効</label>
-                        </td>
-                    </tr>
+                        </p>
+                    </div>
                     <!-- デバッグ用オプション ここまで -->
 
-                </table>
+                </div>
 
 
 
                 <h2>実行</h2>
+                <p>このジェネレータでは引数はデフォルト値ではない場合のみ指定するようになっています。</p>
                 <textarea id="output" disabled></textarea><br>
                 <input type="button" value="生成" onClick="startgen()">
             </form>
