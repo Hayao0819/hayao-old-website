4e7a8cf798230af3477bbb95a236f28152155b5d
hayao <hayao@fascode.net>
2021-02-18 17:57:12 +0900

    [update] : ヘッダーのレイアウトを変更

--- buildmydist-2/script/footer.js
+++ buildmydist-2/script/footer.js
@@ -2,7 +2,7 @@ function FooterBlank(){
     $("#footerblank").css({
         paddingBottom: $("footer").outerHeight() + "px",
     });
-    console.log($("footer").outerHeight() + "px")
+    //console.log($("footer").outerHeight() + "px")
 }
 
 window.addEventListener("load", FooterBlank);
