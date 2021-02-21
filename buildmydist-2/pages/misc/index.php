aca5c678f2425520882774e55aab128ce89da530
hayao <hayao@fascode.net>
2021-02-21 11:17:29 +0900

    [update] : スマホ用のサイドメニューを追加

--- buildmydist-2/pages/misc/index.php
+++ buildmydist-2/pages/misc/index.php
@@ -21,6 +21,13 @@
         <h2>このページは何？</h2>
         <p>Linuxのカスタマイズをする上で必要な情報をまとめています。</p>
         <p>ようは開発をする上で便利なリンク集です。</p>
+        <?php
+            $relation = "${_SERVER['DOCUMENT_ROOT']}/buildmydist-2/pages/$distro/relation.php";
+            if(file_exists($relation)){
+                echo "<h2>リンク集</h2>";
+                include($relation);
+            }
+        ?>
     </main>
 
     <?php include("${commonhtml}/aftermain.php"); ?>
