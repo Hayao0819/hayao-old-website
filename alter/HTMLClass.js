3cfeb32bec2ee1d6e127e6a94aed71b086b4f9fd
hayao <hayao@fascode.net>
2021-07-16 22:02:38 +0900

    [update] : いろいろ更新

--- alter/HTMLClass.js
+++ alter/HTMLClass.js
@@ -1,15 +1,17 @@
 // AddclassToElements(elements, class1, class2 ...)
 function AddClassToElements(Elements){
-    Array.from(Elements).forEach((element)=>{
+    window.addEventListener("load", ()=>{Array.from(Elements).forEach((element)=>{
         let ArgumentList = Array.from(arguments);
-        ArgumentList.shift();
-        ArgumentList.forEach((className) => {
-            element.classList.add(className);
-        })
-    });
+            ArgumentList.shift();
+            ArgumentList.forEach((className) => {
+                element.classList.add(className);
+            })
+        });
+    })
 }
 
-AddClassToElements(document.getElementsByTagName("select"), "selectform-select", "block", "w-3/4", "text-black")
-AddClassToElements(document.getElementsByTagName("h2"), "bg-green-900", "p-2", "rounded-full", "pl-5", "font-bold")
-AddClassToElements(document.getElementsByTagName("h3"), "bg-green-900", "p-2", "m-2" , "rounded")
-AddClassToElements(document.getElementById("main").getElementsByTagName("p"), "p-2", "pl-5", "font-medium")
+AddClassToElements(document.getElementsByTagName("select"), "selectform-select", "block", "w-3/4", "text-black");
+AddClassToElements(document.getElementsByTagName("h2"), "bg-green-900", "p-2", "rounded-full", "pl-5", "font-bold");
+AddClassToElements(document.getElementsByTagName("h3"), "bg-green-900", "p-2", "m-2" , "rounded");
+AddClassToElements(Main.getElementsByTagName("p"), "p-2", "pl-5", "font-medium");
+AddClassToElements(Menu.getElementsByTagName("li"), "block", "px-8", "py-2", "hover:bg-green-300", "hover:text-black", "hover:underline");
