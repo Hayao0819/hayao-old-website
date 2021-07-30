f8da94f8aa20d4d29c974f82c9f17bd0d8d03bed
hayao <hayao@fascode.net>
2021-07-30 22:03:07 +0900

    [update] : コメントを追加

--- sanmoku/script/init.js
+++ sanmoku/script/init.js
@@ -1,19 +1,22 @@
+// 初期化をする関数
 const InitilizeTable = () => {
-    for (a = 0 ; a < TableYNumber; a++){
-        //alert("None")
+    for (a = 0 ; a < TableYNumber; a++){ // 行を作成するループ
+        
+        // 行を作成する
         let tr = document.createElement("tr")
-        for(b = 0; b < TableXNumber; b++){
 
-            // マス目を作成
-            let td = document.createElement("td");
-            td.innerText = InitialStr;
-            td.dataset.clicked = false;
-            td.addEventListener("click", ClickedBox)
-            tr.appendChild(td)
+        // 先程作成した行の中で横並びのボックスを作る
+        for(b = 0; b < TableXNumber; b++){
+            let td = document.createElement("td"); // ボックスを作成
+            td.innerText = InitialStr; // ボックスの文字を初期化する（空白に設定する）
+            td.dataset.clicked = false; // ボックスの状態を「クリックされていない」にする
+            td.addEventListener("click", ClickedBox) //ボックスがクリックされた時「ClickedBox」関数を実行する
+            tr.appendChild(td) //行に先程作成したボックスを挿入する
         }
-        MainTable.appendChild(tr)
+        MainTable.appendChild(tr) //完成した行を画面に挿入する
     }
 }
 
+// 初回読み込み時に初期化を行う
 window.addEventListener("load", InitilizeTable)
 
--- sanmoku/script/main.js
+++ sanmoku/script/main.js
@@ -1,21 +1,22 @@
+// ボックスがクリックされたときの処理
 const ClickedBox = (e) => {
-    const MySelf = e.path[0]
+    const MySelf = e.path[0] // クリックされたボックスを取得する
 
-    if (! GameEnded){
+    if (! GameEnded){ //もしゲームの状態が「終了」でなければ
 
         if (MySelf.dataset.clicked != "true"){
             // クリックされたボックスの設定
-            MySelf.innerText = PlayerMarks[CurrentPlayer];
-            MySelf.dataset.clicked = "true";
+            MySelf.innerText = PlayerMarks[CurrentPlayer]; //クリックされたボックスのテキストをクリックしたプレーヤーの記号にする
+            MySelf.dataset.clicked = "true"; //ボックスの状態を「クリック済み」にする
 
             // プレイヤーを変更する
             ChangePlayer();
 
             // すべてチェックされた場合の処理
             CheckedBox++;
-            if (CheckedBox == TableXNumber * TableYNumber){
-                Msg.innerText = "終了"
-                GameEnded = true;
+            if (CheckedBox == TableXNumber * TableYNumber){  // もしチェックされた数とマス目の数が等しいならば
+                Msg.innerText = "終了" // メッセージに終了と表示
+                GameEnded = true; // ゲームの状態を「終了」にする
             }
         }else{
             console.log("既にクリックされています")
@@ -23,15 +24,17 @@ const ClickedBox = (e) => {
     }
 }
 
+// プレーヤーを変更する処理
 const ChangePlayer = () => {
-    if (CurrentPlayer == PlayerNumer - 1){
-        CurrentPlayer = 0;
+    if (CurrentPlayer == PlayerNumer - 1){ // 今のプレーヤーが最後だったら
+        CurrentPlayer = 0; //最初のプレーヤーに戻る
     }else{
-        CurrentPlayer++;
+        CurrentPlayer++; //次のプレーヤーにする
     }
-    console.log("現在のプレイヤーは" + CurrentPlayer + "です")
+    console.log("現在のプレイヤーは" + CurrentPlayer + "です") //ログを表示する
 }
 
+// 最初にのみログを表示する
 window.addEventListener("load", ()=>{
     console.log(CurrentPlayer + "でゲームをスタートします")
 })
