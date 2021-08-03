7f8ca3f0ad7f518cdc40cd9e6d777b9664590edd
hayao <hayao@fascode.net>
2021-08-04 00:26:10 +0900

    [update] : 判定関数を実装したかった

--- sanmoku/script/init.js
+++ sanmoku/script/init.js
@@ -1,15 +1,25 @@
 // 初期化をする関数
 const InitilizeTable = () => {
-    for (a = 0 ; a < TableYNumber; a++){ // 行を作成するループ
+    for (y = 0 ; y < TableYNumber; y++){ // 行を作成するループ
         
         // 行を作成する
         let tr = document.createElement("tr")
 
         // 先程作成した行の中で横並びのボックスを作る
-        for(b = 0; b < TableXNumber; b++){
-            let td = document.createElement("td"); // ボックスを作成
+        for(x = 0; x < TableXNumber; x++){
+            // ボックスを作成
+            let td = document.createElement("td"); 
+
+            // ボックスの設定
             td.innerText = InitialStr; // ボックスの文字を初期化する（空白に設定する）
             td.dataset.clicked = false; // ボックスの状態を「クリックされていない」にする
+
+            // 座標を設定
+            td.dataset.x = x;
+            td.dataset.y = y;
+
+
+            // ボックスを表示
             td.addEventListener("click", ClickedBox) //ボックスがクリックされた時「ClickedBox」関数を実行する
             tr.appendChild(td) //行に先程作成したボックスを挿入する
         }
--- sanmoku/script/main.js
+++ sanmoku/script/main.js
@@ -1,4 +1,5 @@
 // ボックスがクリックされたときの処理
+// init.jsでボタンがクリックされた場合にClickedBox関数を実行するように指示している
 const ClickedBox = (e) => {
     const MySelf = e.path[0] // クリックされたボックスを取得する
 
@@ -8,9 +9,13 @@ const ClickedBox = (e) => {
             // クリックされたボックスの設定
             MySelf.innerText = PlayerMarks[CurrentPlayer]; //クリックされたボックスのテキストをクリックしたプレーヤーの記号にする
             MySelf.dataset.clicked = "true"; //ボックスの状態を「クリック済み」にする
+            MySelf.dataset.player  = CurrentPlayer; // チェックしたプレーヤーのIDをボックスに書き込む
 
             // プレイヤーを変更する
             ChangePlayer();
+
+            // 勝利判定を行う
+            Judgement();
 
             // すべてチェックされた場合の処理
             CheckedBox++;
@@ -38,3 +43,45 @@ const ChangePlayer = () => {
 window.addEventListener("load", ()=>{
     console.log(CurrentPlayer + "でゲームをスタートします")
 })
+
+// 配列の重複した値が指定回数以上ならTrueを返す
+// 使い方: CountArrayValue(配列, 回数)
+// 参考: https://teratail.com/questions/98451
+const CountArrayValue = (array, count) => {
+    // あとで実装する
+}
+
+// 勝敗の判定 ジャッジメントですの！
+const Judgement = () => {
+    // プレーヤーごとに判定
+    // プレーヤーIDでループし、変数pに今チェック対象のプレーヤーIDが代入される
+    for (p=0; p < PlayerNumer; p++){
+        // チェックしたプレーヤーのIDが書き込まれたボックスの一覧を取得
+        const elements = Array.from(MainTable.querySelectorAll("[data-player]"));
+
+        // 今チェック中のプレーヤーのチェックしたボックスの座標
+        CheckedByCurrentPlayerX = [];
+        CheckedByCurrentPlayerY = [];
+
+        // 上で取得したボックスの一覧1つ1つで確認処理を行う
+        elements.forEach((e) => {
+
+            if (e.dataset.player == p){ // もしいま確認しているボックスが今確認しているプレーヤーIDと一致したら
+
+                // チェックされているボックスの座標を座標リストに追加する
+                CheckedByCurrentPlayerX.push(e.dataset.x);
+                CheckedByCurrentPlayerY.push(e.dataset.y);
+            }
+        });
+
+        // X軸方向の確認
+        //if ()
+        console.log(CountArrayValue(CheckedByCurrentPlayerX, TableXNumber))
+
+        // ログ表示
+        //console.log(p + "のチェック済みのX座標")
+        //console.log(CheckedByCurrentPlayerX)
+        //console.log(p + "のチェック済みのY座標")
+        //console.log(CheckedByCurrentPlayerY)
+    }
+}
