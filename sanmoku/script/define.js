505119760535355f831bf1b10afa054b5c35f39d
hayao <hayao@fascode.net>
2021-08-07 11:58:45 +0900

    [fix] : Use var insead of const let

--- sanmoku/script/define.js
+++ sanmoku/script/define.js
@@ -1,57 +1,57 @@
 // 横のマス目の数
-let TableXNumber = 3;
+var TableXNumber = 3;
 
 // 縦のマス目の数
-let TableYNumber = 3;
+var TableYNumber = 3;
 
 // マス目の初期値
-const InitialStr = "　";
+var InitialStr = "　";
 
 // プレイヤーの記号
-const PlayerMarks = ["◯", "✗" , "△"];
+var PlayerMarks = ["◯", "✗" , "△"];
 
 // プレイヤーの人数（現在 2のみ指定可能）
-let PlayerNumer = 2;
+var PlayerNumer = 2;
 
 // 現在のプレイヤー
-let CurrentPlayer = 0;
+var CurrentPlayer = 0;
 
 //// 以下プログラム用の変数定義につき変更禁止
 
 
 // Main
-const Main = document.getElementById("main");
+var Main = document.getElementById("main");
 
 // HTML
-const HTML = document.getElementsByName("html")[0];
+var HTML = document.getElementsByName("html")[0];
 
 // メインテーブル
-const MainTable = document.getElementById("maintable")
+var MainTable = document.getElementById("maintable")
 
 // メッセージを表示する場所
-const Msg = document.getElementById("msg");
+var Msg = document.getElementById("msg");
 
 // 現在の設定を表示する場所
-const CurrentConfig = document.getElementById("currentconfig")
+var CurrentConfig = document.getElementById("currentconfig")
 
 // リセットボタン
-const ResetButton = document.getElementById("reset");
+var ResetButton = document.getElementById("reset");
 
 // 適用ボタン
-const ApplyButton = document.getElementById("apply");
+var ApplyButton = document.getElementById("apply");
 
 // 設定フォーム: 縦のマス目
-const InputTableX = document.getElementById("inputtablex")
+var InputTableX = document.getElementById("inputtablex")
 
 // 設定フォーム: 横のマス目
-const InputTableY = document.getElementById("inputtabley")
+var InputTableY = document.getElementById("inputtabley")
 
 // 設定フォーム: プレーヤーの人数
-const InputPlayerNumber = document.getElementById("inputplayernumber")
+var InputPlayerNumber = document.getElementById("inputplayernumber")
 
 // チェック済みのボックスの数
-let CheckedBox = 0;
+var CheckedBox = 0;
 
 // ゲームが終了してるかどうか
 // これがtrueの場合はクリックしてもマークをつけない
-let GameEnded = false;
+var GameEnded = false;
