69b15d9ddfebb72e46395e4002559405acaa5cd3
Hayao0819 <Hayao0819@users.noreply.github.com>
2021-10-18 14:42:35 +0000

    deploy: 71ed14973b0f8504930b7a231e145fe352cec001

--- blog/new-post.sh
+++ blog/new-post.sh
@@ -9,4 +9,4 @@ script_path="$( cd -P "$( dirname "$(readlink -f "${0}")" )" && pwd )"
 cd "${script_path}" || return 0
 filename="posts/$(date +%Y%m%d)/${1}/index.md"
 [[ ! -f "${script_path}/${filename}" ]] && hugo new "${filename}"
-type code 1>/dev/null 2>&1 && code "${script_path}/${filename}"
+type code 1>/dev/null 2>&1 && code "${script_path}/src/content/${filename}"
