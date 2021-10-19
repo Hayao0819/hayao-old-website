9087b80f426c77adc9f316256c08804041943484
hayao <hayao@fascode.net>
2021-10-19 14:28:45 +0900

    [add] : ブログを更新

--- blog/new-post.sh
+++ blog/new-post.sh
@@ -4,7 +4,7 @@ set -eu
 
 script_path="$( cd -P "$( dirname "$(readlink -f "${0}")" )" && pwd )"
 
-[[ -z "${1-""}" ]] && echo "引数としてページ名を英語で短く指定してください" >&2 && exit 1
+[[ -z "${1-""}" ]] && echo "Set English short title for argument" >&2 && exit 1
 
 cd "${script_path}" || return 0
 filename="posts/$(date +%Y%m%d)/${1}/index.md"
