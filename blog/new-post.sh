e41a4a1a393a5155de11c044c39a74a1b37c513c
hayao <hayao@fascode.net>
2021-11-18 17:59:43 +0900

    [add] : 更新用スクリプトにLinux用GUIを追加

--- blog/new-post.sh
+++ blog/new-post.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-set -eu
-
-script_path="$( cd -P "$( dirname "$(readlink -f "${0}")" )" && pwd )"
-
-[[ -z "${1-""}" ]] && echo "Set English short title for argument" >&2 && exit 1
-
-cd "${script_path}" || return 0
-filename="posts/$(date +%Y%m%d)/${1}/index.md"
-[[ ! -f "${script_path}/${filename}" ]] && hugo new "${filename}"
-type code 1>/dev/null 2>&1 && code "${script_path}/src/content/${filename}"
+set -Eeu
+title="$(zenity --forms --add-entry="Title" --text="Set English short title for argument" | tr " " "-" | tr "[:upper:]" "[:lower:]")"
+bash "$(cd "$(dirname "${0}")" && pwd)/new-post-cli.sh" "${title}"
