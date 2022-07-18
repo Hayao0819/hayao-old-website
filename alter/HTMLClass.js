5531c86b8f3ba60df15f430f92887292b653bcc0
Hayao0819 <Hayao0819@users.noreply.github.com>
2022-07-18 01:52:50 +0000

    deploy: b2230a642a07cc7d0343e0cf2125ad7408dcc2ec

--- alter/HTMLClass.js
+++ alter/HTMLClass.js
@@ -13,12 +13,15 @@ function AddClassToElements(Elements){
 }
 
 AddClassToElements(document.getElementsByTagName("select"), "selectform-select", "block", "w-3/4", "text-black");
-AddClassToElements(document.getElementsByTagName("h2"), `bg-${Color1}`, "p-2", "rounded-full", "pl-5", "font-bold");
-AddClassToElements(document.getElementsByTagName("h3"), `bg-${Color1}`, "p-2", "m-2" , "rounded");
+AddClassToElements(document.getElementsByTagName("header"), `text-${TextColor2}`);
+AddClassToElements(document.getElementsByTagName("h2"), `bg-${Color1}`, `text-${TextColor2}` ,  "p-2", "rounded-full", "pl-5", "font-bold");
+AddClassToElements(document.getElementsByTagName("h3"), `bg-${Color1}`, `text-${TextColor2}`,  "p-2", "m-2" , "rounded");
 AddClassToElements(Main.getElementsByTagName("p"), "p-2", "pl-5", "font-medium");
-AddClassToElements(Menu.getElementsByTagName("li"), "block", `hover:bg-${Color3}`, "hover:text-black", "hover:underline");
+AddClassToElements(Menu.getElementsByTagName("li"), "block", `hover:bg-${Color3}`, `text-${TextColor2}` ,  `hover:text-${TextColor3}`, "hover:underline");
 AddClassToElements(Menu.getElementsByTagName("li"), "px-3", "py-2");
 AddClassToElements(menu.getElementsByTagName("a"), "block")
+AddClassToElements(document.getElementsByTagName("select"), `bg-${Color1}`, `text-${TextColor2}`);
+AddClassToElements(document.body, `text-${TextColor1}`);
 
 document.getElementById("header_container").classList.add(`bg-${Color1}`);
 document.getElementsByTagName("body")[0].classList.add(`bg-${Color2}`);
