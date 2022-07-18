d3b8bc04208c1f577fae2e21c45568d71b2a1fbe
hayao <hayao@fascode.net>
2022-07-18 10:40:01 +0900

    Update: Write color config in Config.js

--- alter/Config.js
+++ alter/Config.js
@@ -5,3 +5,20 @@ const FixedHeader = false;
 
 // ウェブサイトのタイトル
 const TitleStr = "Alter Linux Alpha Release"
+
+/*
+    Color1: ヘッダー、タイトルの背景色 
+    Color2: bodyの背景色
+    Color3: ホバー時の背景色
+    TextColor1: 本文の文字色
+    TextColor2: ヘッダー、タイトルの文字色
+    TextColor3: ホバー時の文字色
+*/
+
+const CommonColor = "red";
+const Color1 = `${CommonColor}-400`
+const Color2 = `white`
+const Color3 = `${CommonColor}-500` 
+const TextColor1="black";
+const TextColor2="white";
+const TextColor3="white";
--- alter/Define.js
+++ alter/Define.js
@@ -24,26 +24,4 @@ const MetaTitle = document.getElementById("meta_title");
 // ヘッダー内のタイトル
 const HeaderTitle = document.getElementById("header_title");
 
-// 色
 
-
-
-/*
-    Color1: ヘッダー、タイトルの背景色 
-    Color2: bodyの背景色
-    Color3: ホバー時の背景色
-    TextColor1: 本文の文字色
-    TextColor2: ヘッダー、タイトルの文字色
-    TextColor3: ホバー時の文字色
-*/
-
-//const Color1 = "black", Color2 = "white" , Color3 = "green-300"
-
-const CommonColor = "red";
-const Color1 = `${CommonColor}-400`
-const Color2 = `white`
-const Color3 = `${CommonColor}-500` 
-
-const TextColor1="black";
-const TextColor2="white";
-const TextColor3="white";
