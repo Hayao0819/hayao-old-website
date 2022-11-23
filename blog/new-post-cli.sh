b068e49421c3821ce16d6a3a37f23d5c8942992d
hayao <hayao@fascode.net>
2022-11-23 20:10:07 +0900

    Fix: Correct dir name

--- blog/new-post-cli.sh
+++ blog/new-post-cli.sh
@@ -8,6 +8,7 @@ script_path="$(cd "$(dirname "${0}")" || exit 1 ; pwd)"
 [[ -z "${1-""}" ]] && echo "Set English short title for argument" >&2 && exit 1
 
 cd "${script_path}" || return 0
-filename="posts/$(date +%Y%m%d)/${1}/index.md"
+dirname="$(tr " " "-" <<< "$1" | tr "[:upper:]" "[:lower:]")"
+filename="posts/$(date +%Y%m%d)/${dirname}/index.md"
 [[ ! -f "${script_path}/${filename}" ]] && hugo new "${filename}"
 type code 1>/dev/null 2>&1 && code "${script_path}/src/content/${filename}"
