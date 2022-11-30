6e4a4ef49b8bc047f26b289ce54d07145fb0494e
Hayao0819 <Hayao0819@users.noreply.github.com>
2022-11-30 10:08:49 +0000

    deploy: d84af64ac55ff1d9d18386e0737e1dc77e6ff0b1

--- blog/new-post-cli.sh
+++ blog/new-post-cli.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -eu
 
 #script_path="$( cd -P "$( dirname "$(readlink -f "${0}")" )" && pwd )"
 script_path="$(cd "$(dirname "${0}")" || exit 1 ; pwd)"
 
-[[ -z "${1-""}" ]] && echo "Set English short title for argument" >&2 && exit 1
+[ -z "${1-""}" ] && echo "Set English short title for argument" >&2 && exit 1
 
 cd "${script_path}" || return 0
-dirname="$(tr " " "-" <<< "$1" | tr "[:upper:]" "[:lower:]")"
+dirname="$(echo "$1" | tr " " "-" | tr "[:upper:]" "[:lower:]")"
 filename="posts/$(date +%Y%m%d)/${dirname}/index.md"
-[[ ! -f "${script_path}/${filename}" ]] && hugo new "${filename}"
+[ ! -f "${script_path}/${filename}" ] && hugo new "${filename}"
 type code 1>/dev/null 2>&1 && code "${script_path}/src/content/${filename}"
